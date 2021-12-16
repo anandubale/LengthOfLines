553e8bb16092588e33c13d6fab20be21df91df6a
anandubale <au26199@gmail.com>
2021-12-16 15:52:13 +0530

    added Program displaying welcome to line COmputatioin Problem

---
+++ IntroductionToLineProblems/IntroductionToLineProblems/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IntroductionToLineProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcoome to Line COmparison Computation Problem to Master Branch");
+        }
+    }
+}
